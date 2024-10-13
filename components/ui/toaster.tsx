adb11ae494e2345d00d050ff16164373122faffc
sahilkakwani9 <sahkakwani@gmail.com>
2024-10-13 13:53:36 +0530

    fix: create swap order

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-
 import {
   Toast,
   ToastClose,
---
+++ lib/utils/createSwapOrder.ts
@@ -0,0 +1,47 @@
+type createOrderParams = {
+  owner: string;
+  inputMint: string;
+  outputMint: string;
+  amount: string;
+};
+
+export default async function createOrder(params: createOrderParams) {
+  try {
+    console.log(params, "params");
+    const quoteResponse = await (
+      await fetch(
+        `https://quote-api.jup.ag/v6/quote?inputMint=${params.inputMint}\
+  &outputMint=${params.outputMint}\
+  &amount=${params.amount}\
+  &slippageBps=50`
+      )
+    ).json();
+
+    console.log(quoteResponse, "response from qoute");
+
+    // get serialized transactions for the swap
+    const { swapTransaction } = await (
+      await fetch("https://quote-api.jup.ag/v6/swap", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          // quoteResponse from /quote api
+          quoteResponse,
+          // user public key to be used for the swap
+          userPublicKey: params.owner,
+          // auto wrap and unwrap SOL. default is true
+          wrapAndUnwrapSol: true,
+          // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
+          // feeAccount: "fee_account_public_key"
+        }),
+      })
+    ).json();
+    console.log("swap transaction", swapTransaction);
+
+    return swapTransaction;
+  } catch (error) {
+    console.log("error creating swap order", error);
+  }
+}
