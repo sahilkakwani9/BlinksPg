a82155605fb357d666f25f8a7b323eee09c031ca
sahilkakwani9 <sahkakwani@gmail.com>
2025-03-07 14:42:03 +0530

    fix: assets converted to webp

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -10,49 +10,49 @@ function page() {
           title="Swap Tokens"
           desc="Build a blink that swaps a specific token to another token using
           Jupiter"
-          posterUri="/images/covers/Swap.png"
+          posterUri="/images/covers/Swap.webp"
           navigateTo="/jup-swap"
         />
         <TemplateCard
           title="Send me TIP"
           desc="Build a blink that sends TIP(USDC) to any adddress"
-          posterUri="/images/covers/Tip.png"
+          posterUri="/images/covers/Tip.webp"
           navigateTo="/tip"
         />
         <TemplateCard
           title="Create Polls"
           desc="Build a blink that lets users interact with a poll"
-          posterUri="/images/covers/Poll.png"
+          posterUri="/images/covers/Poll.webp"
           navigateTo="/poll"
         />
         <TemplateCard
           title="Create NFT through Metaplex"
           desc="Build a blink that lets users create NFTs through Metaplex"
-          posterUri="/images/covers/NFT-Bid.png"
+          posterUri="/images/covers/NFT-Bid.webp"
           navigateTo="/nft"
         />
         <TemplateCard
           title="List NFT's through Tensor"
           desc="Build a blink that lets users list their NFT on Tensor"
-          posterUri="/images/covers/List.png"
+          posterUri="/images/covers/List.webp"
           navigateTo="/list"
         />
         <TemplateCard
           title="Sell NFT's on Tensor"
           desc="Build a blink that lets users sell their NFT's on Tensor"
-          posterUri="/images/covers/Sell.png"
+          posterUri="/images/covers/Sell.webp"
           navigateTo="/sell"
         />
         <TemplateCard
           title="Stake in Meteora Pools"
           desc="Stake your Money in Meteora Liquidity Pools"
-          posterUri="/images/covers/Meteora.png"
+          posterUri="/images/covers/Meteora.webp"
           navigateTo="/stake"
         />
-          <TemplateCard
+        <TemplateCard
           title="Stake your Sol"
           desc="Stake your Sol through Native Staking"
-          posterUri="/images/covers/Stake.png"
+          posterUri="/images/covers/Stake.webp"
           navigateTo="/validator"
         />
       </div>
